88e9730287ecf19b58df2a6b602f128fe6d97901
Igor Ferreira Calbo <47559004+IgorCalbo@users.noreply.github.com>
2022-06-17 18:41:49 -0300

    inserindo arquivos

---
+++ requisicao/req.py
@@ -0,0 +1,12 @@
+import requests
+
+link = 'https://minhaapi.igorcalbo1.repl.co/pegarvendas'
+
+requisicao = requests.get(link)
+
+print(requisicao)
+print(requisicao.json())
+
+# dicionario = requisicao.json()
+# print(dicionario['total_vendas'])
+
